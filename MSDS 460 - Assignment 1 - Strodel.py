bd016e690dc50a7c5022bc611e356ea640132d27
Forrest Strodel <forreststrodel2027@u.northwestern.edu>
2023-09-24 20:52:06 -0400

    Initial commit for Assignment 1

---
+++ MSDS 460 - Assignment 1 - Strodel.py
@@ -0,0 +1,94 @@
+# Import packages
+import pulp
+from pulp import LpVariable, LpProblem, LpMaximize, LpStatus, value, LpMinimize
+
+# Create function that takes cost and servings per container and calculates cost per serving (cps)
+def cost_per_serving(cost, servings):
+    return round(cost / servings, 2)
+
+# Calculate unit costs for each food item: eggs, milk, vitamins, vegetables, cheerios
+# Eggs
+cps_eggs = cost_per_serving(1.49, 12)
+
+# milk
+cps_milk = cost_per_serving(1.89, 8)
+
+# vitamins (using cost divided by two because these are always buy-one-get-one)
+cps_vit = cost_per_serving(18.99 / 2, 75)
+
+# vegetables
+cps_veg = cost_per_serving(2.59, 10)
+
+# cheerios
+cps_cheerios = cost_per_serving(3.69, 6)
+
+# print all cost per servings
+print(f'Eggs: {cps_eggs} per serving\nMilk: {cps_milk} per serving\nVitamins: {cps_vit} per serving\nVegetables: {cps_veg} per serving\nCheerios: {cps_cheerios} per serving')
+
+# define variables
+eggs = LpVariable('Eggs', 0, None) # eggs >= 0
+milk = LpVariable('Milk', 0, None) # milk >= 0
+vits = LpVariable('Vitamins', 0, None) # vitamins >=0
+vegs = LpVariable('Vegetables', 0, None) # vegetables >= 0
+chrs = LpVariable('Cheerios', 0, None) # cheerios >= 0
+
+# define problem
+prob = LpProblem('problem', LpMinimize) # minimize cost
+
+# define constraints
+prob += eggs * 70 + milk * 115 + vits *  0 + vegs *  10 + chrs * 190 <= 5000 # milligrams of sodium
+prob += eggs * 70 + milk * 120 + vits * 15 + vegs *  60 + chrs * 140 >= 2000 # calories
+prob += eggs *  6 + milk *   8 + vits *  0 + vegs *   2 + chrs *   5 >=   50 # grams of protein
+prob += eggs *  1 + milk *   3 + vits * 40 + vegs *   0 + chrs *   4 >=   20 # micrograms of Vitamin D
+prob += eggs * 30 + milk * 292 + vits *  0 + vegs *   0 + chrs * 130 >= 1300 # milligrams of calcium
+prob += eggs *  1 + milk *   0 + vits *  0 + vegs *   1 + chrs *  13 >=   18 # milligrams of iron
+prob += eggs * 70 + milk * 341 + vits *  0 + vegs * 171 + chrs * 250 >= 4700 # milligrams of potassium
+
+# define objective function
+prob += cps_eggs * eggs + cps_milk * milk + cps_vit * vits + cps_veg * vegs + cps_cheerios * chrs
+
+# solve the problem
+status = prob.solve()
+print(f'Diet Problem\nStatus: {LpStatus[status]}')
+
+# print results
+print(f'Servings of:')
+for variable in prob.variables():
+    print(f'  - {variable.name} = {round(variable.varValue, 2)}')
+print(f'Minimized Cost: {round(value(prob.objective), 2)}')
+
+# define the revised problem (with additional constraints)
+# define variables
+eggs = LpVariable('Eggs', 0, None) # eggs >= 0
+milk = LpVariable('Milk', 0, None) # milk >= 0
+vits = LpVariable('Vitamins', 0, None) # vitamins >=0
+vegs = LpVariable('Vegetables', 0, None) # vegetables >= 0
+chrs = LpVariable('Cheerios', 0, None) # cheerios >= 0
+
+# define problem
+prob_rev = LpProblem('problem', LpMinimize) # minimize cost
+
+# define constraints
+prob_rev += eggs * 70 + milk * 115 + vits *  0 + vegs *  10 + chrs * 190 <= 5000 # milligrams of sodium
+prob_rev += eggs * 70 + milk * 120 + vits * 15 + vegs *  60 + chrs * 140 >= 2000 # calories
+prob_rev += eggs *  6 + milk *   8 + vits *  0 + vegs *   2 + chrs *   5 >=   50 # grams of protein
+prob_rev += eggs *  1 + milk *   3 + vits * 40 + vegs *   0 + chrs *   4 >=   20 # micrograms of Vitamin D
+prob_rev += eggs * 30 + milk * 292 + vits *  0 + vegs *   0 + chrs * 130 >= 1300 # milligrams of calcium
+prob_rev += eggs *  1 + milk *   0 + vits *  0 + vegs *   1 + chrs *  13 >=   18 # milligrams of iron
+prob_rev += eggs * 70 + milk * 341 + vits *  0 + vegs * 171 + chrs * 250 >= 4700 # milligrams of potassium
+prob_rev += eggs * 15 + milk *   0 + vits * 28 + vegs *   0 + chrs *   0 >=   55 # micrograms of selenium
+prob_rev += eggs *  1 + milk *   0 + vits *  6 + vegs *   0 + chrs *   2 >=   11 # milligrams of zinc
+prob_rev += eggs *  0 + milk *   0 + vits * 18 + vegs *   0 + chrs *   0 >=   35 # micrograms of chromium
+
+# define objective function
+prob_rev += cps_eggs * eggs + cps_milk * milk + cps_vit * vits + cps_veg * vegs + cps_cheerios * chrs
+
+# solve the problem
+status = prob_rev.solve()
+print(f'Diet Problem\nStatus: {LpStatus[status]}')
+
+# print results
+print(f'Servings of:')
+for variable in prob_rev.variables():
+    print(f'  - {variable.name} = {round(variable.varValue, 2)}')
+print(f'Minimized Cost: {round(value(prob_rev.objective), 2)}')
