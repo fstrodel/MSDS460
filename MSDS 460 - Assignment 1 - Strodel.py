c520346dee9f90386304e7d965238470a6f50049
Forrest Strodel <forreststrodel2027@u.northwestern.edu>
2023-09-28 22:35:33 -0400

    Second revision on 9/27

--- MSDS 460 - Assignment 1 - Strodel.py
+++ MSDS 460 - Assignment 1 - Strodel.py
@@ -23,7 +23,7 @@ cps_veg = cost_per_serving(2.59, 10)
 cps_cheerios = cost_per_serving(3.69, 6)
 
 # print all cost per servings
-print(f'Eggs: {cps_eggs} per serving\nMilk: {cps_milk} per serving\nYogurt: {cps_vit} per serving\nVegetables: {cps_veg} per serving\nCheerios: {cps_cheerios} per serving')
+print(f'Eggs: {cps_eggs} per serving\nMilk: {cps_milk} per serving\nYogurt: {cps_yog} per serving\nVegetables: {cps_veg} per serving\nCheerios: {cps_cheerios} per serving')
 
 # define variables
 eggs = LpVariable('Eggs', 0, None) # eggs >= 0
@@ -36,13 +36,13 @@ chrs = LpVariable('Cheerios', 0, None) # cheerios >= 0
 prob = LpProblem('problem', LpMinimize) # minimize cost
 
 # define constraints
-prob += eggs * 70 + milk * 115 + yogt *  60 + vegs *  10 + chrs * 190 <= 5000 # milligrams of sodium
-prob += eggs * 70 + milk * 120 + yogt * 130 + vegs *  60 + chrs * 140 >= 2000 # calories
-prob += eggs *  6 + milk *   8 + yogt *  14 + vegs *   2 + chrs *   5 >=   50 # grams of protein
-prob += eggs *  1 + milk *   3 + yogt *   0 + vegs *   0 + chrs *   4 >=   20 # micrograms of Vitamin D
-prob += eggs * 30 + milk * 292 + yogt * 170 + vegs *   0 + chrs * 130 >= 1300 # milligrams of calcium
-prob += eggs *  1 + milk *   0 + yogt *   0 + vegs *   1 + chrs *  13 >=   18 # milligrams of iron
-prob += eggs * 70 + milk * 341 + yogt * 220 + vegs * 171 + chrs * 250 >= 4700 # milligrams of potassium
+prob += 70 * eggs + 115 * milk +  60 * yogt +  10 * vegs + 190 * chrs <= 5000 # milligrams of sodium
+prob += 70 * eggs + 120 * milk + 130 * yogt +  60 * vegs + 140 * chrs >= 2000 # calories
+prob +=  6 * eggs +   8 * milk +  14 * yogt +   2 * vegs +   5 * chrs >=   50 # grams of protein
+prob +=  1 * eggs +   3 * milk +   0 * yogt +   0 * vegs +   4 * chrs >=   20 # micrograms of Vitamin D
+prob += 30 * eggs + 292 * milk + 170 * yogt +   0 * vegs + 130 * chrs >= 1300 # milligrams of calcium
+prob +=  1 * eggs +   0 * milk +   0 * yogt +   1 * vegs +  13 * chrs >=   18 # milligrams of iron
+prob += 70 * eggs + 341 * milk + 220 * yogt + 171 * vegs + 250 * chrs >= 4700 # milligrams of potassium
 
 # define objective function
 prob += cps_eggs * eggs + cps_milk * milk + cps_yog * yogt + cps_veg * vegs + cps_cheerios * chrs
@@ -69,15 +69,17 @@ chrs = LpVariable('Cheerios', 0, None) # cheerios >= 0
 prob_rev = LpProblem('problem', LpMinimize) # minimize cost
 
 # define constraints
-prob_rev += eggs * 70 + milk * 115 + yogt *  60 + vegs *  10 + chrs * 190 <= 5000 # milligrams of sodium
-prob_rev += eggs * 70 + milk * 120 + yogt * 130 + vegs *  60 + chrs * 140 >= 2000 # calories
-prob_rev += eggs *  6 + milk *   8 + yogt *  14 + vegs *   2 + chrs *   5 >=   50 # grams of protein
-prob_rev += eggs *  1 + milk *   3 + yogt *   0 + vegs *   0 + chrs *   4 >=   20 # micrograms of Vitamin D
-prob_rev += eggs * 30 + milk * 292 + yogt * 170 + vegs *   0 + chrs * 130 >= 1300 # milligrams of calcium
-prob_rev += eggs *  1 + milk *   0 + yogt *   0 + vegs *   1 + chrs *  13 >=   18 # milligrams of iron
-prob_rev += eggs * 70 + milk * 341 + yogt * 220 + vegs * 171 + chrs * 250 >= 4700 # milligrams of potassium
-prob_rev += eggs *  1 + milk *   0 + yogt *   0 + vegs *   0 + chrs *   2 >=   11 # milligrams of zinc
-prob_rev += eggs *  0 + milk *   0 + yogt *  11 + vegs *   0 + chrs *   1 <=   50 # grams of added sugar
+prob_rev += 70 * eggs + 115 * milk +  60 * yogt +  10 * vegs + 190 * chrs <= 5000 # milligrams of sodium
+prob_rev += 70 * eggs + 120 * milk + 130 * yogt +  60 * vegs + 140 * chrs >= 2000 # calories
+prob_rev +=  6 * eggs +   8 * milk +  14 * yogt +   2 * vegs +   5 * chrs >=   50 # grams of protein
+prob_rev +=  1 * eggs +   3 * milk +   0 * yogt +   0 * vegs +   4 * chrs >=   20 # micrograms of Vitamin D
+prob_rev += 30 * eggs + 292 * milk + 170 * yogt +   0 * vegs + 130 * chrs >= 1300 # milligrams of calcium
+prob_rev +=  1 * eggs +   0 * milk +   0 * yogt +   1 * vegs +  13 * chrs >=   18 # milligrams of iron
+prob_rev += 70 * eggs + 341 * milk + 220 * yogt + 171 * vegs + 250 * chrs >= 4700 # milligrams of potassium
+# define added constraints
+prob_rev +=  1 * eggs +   0 * milk +   0 * yogt +   0 * vegs +   2 * chrs >=         11 # milligrams of zinc
+prob_rev +=  0 * eggs +   0 * milk +  11 * yogt +   0 * vegs +   1 * chrs <=         50 # grams of added sugar
+prob_rev += 30 * eggs + 292 * milk + 170 * yogt +   0 * vegs + 130 * chrs <= 1300 * 1.5 # milligrams of calcium
 
 # define objective function
 prob_rev += cps_eggs * eggs + cps_milk * milk + cps_yog * yogt + cps_veg * vegs + cps_cheerios * chrs
@@ -91,5 +93,3 @@ print(f'Servings of:')
 for variable in prob_rev.variables():
     print(f'  - {variable.name} = {round(variable.varValue, 2)}')
 print(f'Minimized Cost: {round(value(prob_rev.objective), 2)}')
-
-#test test test
